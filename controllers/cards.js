78f4d4f3c43b123f0caeee2b8e3f185d7ea679f2
x-borz <KabanovRE@mosreg.ru>
2022-08-08 15:30:14 +0300

    роуты, контроллеры

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,22 +1,31 @@
 const Card = require('../models/card');
+const { NOT_FOUND_CODE, BAD_REQUEST_CODE, DEFAULT_ERROR_CODE } = require('../utils/constants');
 
 const getAllCards = (req, res) => {
   Card.find({})
+    .populate(['owner', 'likes'])
     .then(cards => res.send(cards))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' }));
 }
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
+    .then(card => card.populate('owner'))
     .then(card => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_CODE).send({ message: 'Данные в запросе невалидны' });
+        return;
+      }
+      res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' });
+    });
 }
 
 const deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.userId)
-    .then(card => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then(() => res.send({ message: 'Карточка удалена' }))
+    .catch(() => res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' }));
 }
 
 const addLike = (req, res) => {
@@ -25,8 +34,15 @@ const addLike = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then(card => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .populate(['owner', 'likes'])
+    .then(card => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(NOT_FOUND_CODE).send({ message: 'Запрашиваемая карточка не найдена'});
+      }
+    })
+    .catch(() => res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' }));
 }
 
 const removeLike = (req, res) => {
@@ -35,8 +51,15 @@ const removeLike = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then(card => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .populate(['owner', 'likes'])
+    .then(card => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(NOT_FOUND_CODE).send({ message: 'Запрашиваемая карточка не найдена'});
+      }
+    })
+    .catch(() => res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' }));
 }
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,74 @@
 const User = require('../models/user');
+const { NOT_FOUND_CODE, BAD_REQUEST_CODE, DEFAULT_ERROR_CODE } = require('../utils/constants');
 
 const getAllUsers = (req, res) => {
   User.find({})
     .then(users => res.send(users))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
 
 const getUserById = (req, res) => {
   console.log(req.params);
   User.findById(req.params.userId)
-    .then(user => res.send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then(user => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(NOT_FOUND_CODE).send({ message: 'Запрашиваемый пользователь не найден'});
+      }
+    })
+    .catch(() => res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' }));
 }
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then(user => res.send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_CODE).send({ message: 'Данные в запросе невалидны' });
+        return;
+      }
+      res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' });
+    });
+  }
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
-    .then(user => res.send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then(user => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(NOT_FOUND_CODE).send({message: 'Запрашиваемый пользователь не найден'});
+      }
+    })
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_CODE).send({ message: 'Данные в запросе невалидны' });
+        return;
+      }
+      res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' });
+    });
 }
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
-    .then(user => res.send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    . then(user => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(NOT_FOUND_CODE).send({message: 'Запрашиваемый пользователь не найден'});
+      }
+    })
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_CODE).send({ message: 'Данные в запросе невалидны' });
+        return;
+      }
+      res.status(DEFAULT_ERROR_CODE).send({ message: 'Произошла ошибка' });
+    });
 }
 
 module.exports = {
