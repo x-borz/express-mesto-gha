94f38a5d4e1777a35ac52199831e18a2cfd6cebb
x-borz <KabanovRE@mosreg.ru>
2022-08-08 14:12:40 +0300

    роуты, контроллеры

--- controllers/users.js
+++ controllers/users.js
@@ -21,11 +21,17 @@ const createUser = (req, res) => {
 }
 
 const updateUser = (req, res) => {
-
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
+    .then(user => res.send(user))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
 
 const updateAvatar = (req, res) => {
-
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
+    .then(user => res.send(user))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 }
 
 module.exports = {
