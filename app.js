c0d56d247513b60fca02232fbc8d32a787ab1bb1
x-borz <KabanovRE@mosreg.ru>
2022-08-21 22:41:53 +0300

    jou celebrate validation

--- app.js
+++ app.js
@@ -26,9 +26,13 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().uri(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri({
+      scheme: ['http', 'https'],
+      allowRelative: false,
+      relativeOnly: false,
+    }),
   }),
 }), createUser);
 
