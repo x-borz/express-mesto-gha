369168b585049eca3de2f3904261d26069bf7543
x-borz <KabanovRE@mosreg.ru>
2022-08-21 21:53:09 +0300

    fix http codes

--- routes/card-router.js
+++ routes/card-router.js
@@ -1,12 +1,28 @@
 const cardRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getAllCards, createCard, deleteCard, addLike, removeLike,
 } = require('../controllers/cards');
 
+const cardIdRule = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+});
+
 cardRouter.get('/', getAllCards);
-cardRouter.post('/', createCard);
-cardRouter.delete('/:cardId', deleteCard);
-cardRouter.put('/:cardId/likes', addLike);
-cardRouter.delete('/:cardId/likes', removeLike);
+
+cardRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(8),
+    link: Joi.string().required(),
+  }),
+}), createCard);
+
+cardRouter.delete('/:cardId', cardIdRule, deleteCard);
+
+cardRouter.put('/:cardId/likes', cardIdRule, addLike);
+
+cardRouter.delete('/:cardId/likes', cardIdRule, removeLike);
 
 module.exports = cardRouter;
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,12 +1,26 @@
 const userRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getAllUsers, getUserById, updateUser, updateAvatar, getUser,
 } = require('../controllers/users');
 
 userRouter.get('/', getAllUsers);
+
 userRouter.get('/me', getUser);
-userRouter.patch('/me', updateUser);
-userRouter.patch('/me/avatar', updateAvatar);
+
+userRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri(),
+  }),
+}), updateAvatar);
+
 userRouter.get('/:userId', getUserById);
 
 module.exports = userRouter;
