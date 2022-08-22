b359497acdaa22bc83785bd46dd45d988c538a4c
x-borz <KabanovRE@mosreg.ru>
2022-08-22 12:47:17 +0300

    рефакторинг после 1-го ревью: вынес роуты из app в отдельный файл

--- routes/card-router.js
+++ routes/card-router.js
@@ -5,13 +5,9 @@ const {
 const { validCardId, validCreateCardRequest } = require('../validators/card');
 
 cardRouter.get('/', getAllCards);
-
 cardRouter.post('/', validCreateCardRequest, createCard);
-
 cardRouter.delete('/:cardId', validCardId, deleteCard);
-
 cardRouter.put('/:cardId/likes', validCardId, addLike);
-
 cardRouter.delete('/:cardId/likes', validCardId, removeLike);
 
 module.exports = cardRouter;
