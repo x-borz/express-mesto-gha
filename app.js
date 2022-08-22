6931d113d45f9ba50574f742aed22aea1160c169
x-borz <KabanovRE@mosreg.ru>
2022-08-22 08:54:10 +0300

    fix autotest's errors

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use((err, req, res, next) => {
   res
     .status(statusCode)
     .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
 });
 
 app.listen(PORT);
