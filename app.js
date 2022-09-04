d44aa2b3ec53c26b7d534f76468e21ea39df55ef
Renat Kabanov <x-borz@yandex.ru>
2022-09-04 18:00:13 +0300

    mongo

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT } = require('./utils/constants');
+const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(auth);
+
+app.use(requestLogger);
+
 app.use('/', require('./routes'));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
 
 app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,14 @@
 const router = require('express').Router();
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
 const { login, createUser } = require('../controllers/users');
-const auth = require('../middlewares/auth');
 const { sendNotFoundResponse } = require('../controllers/404');
-const errorHandler = require('../middlewares/error-handler');
 const { validSigninRequest, validSignupRequest } = require('../validators/user');
-
-router.use(helmet());
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/signin', validSigninRequest, login);
 router.post('/signup', validSignupRequest, createUser);
 router.get('/signout', (req, res) => res.clearCookie('jwt').send({ message: 'Выход' }));
-
-router.use(cookieParser());
-router.use(auth);
-
 router.use('/users', require('./user-router'));
 router.use('/cards', require('./card-router'));
 
 router.use('*', sendNotFoundResponse);
-
-router.use(errors());
-
-router.use(errorHandler);
 
 module.exports = router;
